87be5510abba6617193312db8fc14307e9483590
Mateen Ulhaq <mulhaq2005@gmail.com>
2019-08-30 22:33:58 -0700

    RenderScript initial allocation only
    Fixes W/System: A resource failed to call destroy

--- android/app/src/main/java/com/sicariusnoctis/collaborativeintelligence/MainActivity.kt
+++ android/app/src/main/java/com/sicariusnoctis/collaborativeintelligence/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.sicariusnoctis.collaborativeintelligence
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.ImageFormat
 import android.os.Bundle
 import android.renderscript.*
 import android.util.Log
@@ -16,14 +18,23 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private val TAG = MainActivity::class.qualifiedName
-    lateinit var fotoapparat: Fotoapparat
-    lateinit var networkThread: NetworkThread
+    private var isInitRenderScript = false
+    private lateinit var fotoapparat: Fotoapparat
+    private lateinit var networkThread: NetworkThread
+    private lateinit var rs: RenderScript
+    private lateinit var yuvToRgbIntrinsic: ScriptIntrinsicYuvToRGB
+    private lateinit var resizeIntrinsic: ScriptIntrinsicResize
+    private lateinit var inData: Allocation
+    private lateinit var rgbaData: Allocation
+    private lateinit var resizedData: Allocation
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initFotoapparat()
         networkThread = NetworkThread() // TODO No real need to lateinit... unless specifying IP, port
+        rs = RenderScript.create(this)
     }
 
     override fun onStart() {
@@ -90,34 +101,22 @@ class MainActivity : AppCompatActivity() {
 
     // TODO what if processing is too slow? copy frame (meh), then skip frame processor if frame in-processing lock acquired? (or wait? nah...)
     private fun frameProcessor(frame: Frame) {
-        // Log.d(TAG, frame.size.toString())
-        val rgba = rsProcess(this, frame.image, frame.size.width, frame.size.height)
-        // Log.d(TAG, "${rgba.size}")
-        // Log.d(TAG, "${rgba[0]} ${rgba[1]} ${rgba[2]} ${rgba[3]}")
+        if (!isInitRenderScript) {
+            initRenderScript(frame.size.width, frame.size.height)
+        }
+
+        val rgba = rsProcess(frame.image)
         // classifier.fillOutputByteArray(outputTransmittedBytes)
         val outputTransmittedBytes = rgba
         networkThread.writeData(outputTransmittedBytes)
     }
 
     // TODO ensure camera outputs NV21... or YUVxx? What is default setting?
-    // TODO correct orientation...
     // TODO crop?
     // TODO RGBA?
     // TODO reduce rescale aliasing through blur https://medium.com/@petrakeas/alias-free-resize-with-renderscript-5bf15a86ce3
-    private fun rsProcess(context: Context, byteArray: ByteArray, width: Int, height: Int): ByteArray {
-        val rs = RenderScript.create(context)
-
-        val yuvType = Type.createX(rs, Element.U8(rs), byteArray.size)
-        val rgbaType = Type.createXY(rs, Element.RGBA_8888(rs), width, height)
-        val resizeType = Type.createXY(rs, Element.RGBA_8888(rs), 224, 224)
-
-        val inData = Allocation.createTyped(rs, yuvType, Allocation.USAGE_SCRIPT)
-        val rgbaData = Allocation.createTyped(rs, rgbaType, Allocation.USAGE_SCRIPT)
-        val resizedData = Allocation.createTyped(rs, resizeType, Allocation.USAGE_SCRIPT)
-
-        val yuvToRgbIntrinsic = ScriptIntrinsicYuvToRGB.create(rs, Element.U8_4(rs))
-        val resizeIntrinsic = ScriptIntrinsicResize.create(rs)
-
+    // TODO correct orientation... shouldn't ALWAYS be rotating, though...
+    private fun rsProcess(byteArray: ByteArray): ByteArray {
         inData.copyFrom(byteArray)
         yuvToRgbIntrinsic.setInput(inData)
         yuvToRgbIntrinsic.forEach(rgbaData)
@@ -128,23 +127,24 @@ class MainActivity : AppCompatActivity() {
         resizedData.copyTo(outBuffer)
 
         return outBuffer
-
-        //  TODO center crop to square? 224x224
-        //
-        // frameToCropTransform =
-        //     ImageUtils.getTransformationMatrix(
-        //         previewWidth,
-        //         previewHeight,
-        //         classifier.getImageSizeX(),
-        //         classifier.getImageSizeY(),
-        //         sensorOrientation,
-        //         MAINTAIN_ASPECT);
-        //
-        // val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
-        // outData.copyTo(bitmap)
     }
 
-    // TODO W/System: A resource failed to call destroy.
+    private fun initRenderScript(width: Int, height: Int) {
+        val yuvType = Type.createX(rs, Element.U8(rs), width * height * 3 / 2)
+        val rgbaType = Type.createXY(rs, Element.RGBA_8888(rs), width, height)
+        val resizeType = Type.createXY(rs, Element.RGBA_8888(rs), 224, 224)
+
+        inData = Allocation.createTyped(rs, yuvType, Allocation.USAGE_SCRIPT)
+        rgbaData = Allocation.createTyped(rs, rgbaType, Allocation.USAGE_SCRIPT)
+        resizedData = Allocation.createTyped(rs, resizeType, Allocation.USAGE_SCRIPT)
+
+        yuvToRgbIntrinsic = ScriptIntrinsicYuvToRGB.create(rs, Element.U8_4(rs))
+        resizeIntrinsic = ScriptIntrinsicResize.create(rs)
+
+        isInitRenderScript = true
+    }
+
     // TODO E/BufferQueueProducer: [SurfaceTexture-0-22526-3] cancelBuffer: BufferQueue has been abandoned
     // TODO why does the stream freeze on server-side?
+    // TODO run expensive operations in pipeline in parallel
 }
