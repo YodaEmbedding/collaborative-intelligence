32f419fe01cf9a1eb07ef878c6cf2c9e616adf5b
Mateen Ulhaq <mulhaq2005@gmail.com>
2019-09-02 03:36:27 -0700

    Crop now accepts rs_allocation rather than produces

--- android/app/src/main/java/com/sicariusnoctis/collaborativeintelligence/CameraPreviewPostprocessor.kt
+++ android/app/src/main/java/com/sicariusnoctis/collaborativeintelligence/CameraPreviewPostprocessor.kt
@@ -23,8 +23,6 @@ class CameraPreviewPostprocessor {
     private val crop: ScriptC_crop
     private val preprocess: ScriptC_preprocess // TODO rename to more descriptive
 
-    private val cropLaunchOptions: LaunchOptions
-
     // TODO ensure camera outputs NV21... or YUVxx? What is default setting?
     // TODO... write doc-comment
     // YUV -> RGBA
@@ -63,61 +61,21 @@ class CameraPreviewPostprocessor {
         resize = ScriptIntrinsicResize.create(rs)
         preprocess = ScriptC_preprocess(rs)
 
-        val xStart = (width - side) / 2
-        val yStart = (height - side) / 2
-        cropLaunchOptions = LaunchOptions()
-            .setX(xStart, xStart + side)
-            .setY(yStart, yStart + side)
-
         yuvToRGB.setInput(inputAllocation)
+        crop._xStart = ((width - side) / 2).toLong()
+        crop._yStart = ((height - side) / 2).toLong()
+        crop._input = rgbaAllocation
         resize.setInput(cropAllocation)
-        crop._xStart = xStart.toLong()
-        crop._yStart = yStart.toLong()
-        crop._output = cropAllocation
     }
 
     fun process(frame: Frame): ByteArray {
         inputAllocation.copyFrom(frame.image)
         yuvToRGB.forEach(rgbaAllocation)
-        crop.forEach_crop(rgbaAllocation, cropLaunchOptions) // TODO the arguments could be reversed...
+        crop.forEach_crop(cropAllocation)
         resize.forEach_bicubic(resizeAllocation)
         preprocess.forEach_preprocess(resizeAllocation, outputAllocation)
         val byteArray = ByteArray(outputAllocation.bytesSize)
         outputAllocation.copyTo(byteArray)
         return byteArray
     }
-
-    // private fun rsProcess(frame: Frame): ByteArray {
-    //     inputAllocation.copyFrom(frame.image)
-    //
-    //     // TODO do we really need to do this every time...? Maybe if orientation changes, yeah...? idk
-    //     var lo = getScriptLaunchOptions(frame.size.width, frame.size.height)
-    //
-    //     // TODO watch out! the cropped output size is 224 x 224!!
-    //
-    //     scriptHandle.set_xStart(lo.xStart.toLong())
-    //     scriptHandle.set_yStart(lo.yStart.toLong())
-    //     scriptHandle.set_outputWidth(frame.size.width.toLong())
-    //     scriptHandle.set_outputHeight(frame.size.height.toLong())
-    //
-    //     scriptHandle.forEach_preprocess(scriptAllocation, outputAllocation, lo)
-    //
-    //     var outBuffer = ByteArray(outputAllocationRGB.bytesSize)
-    //     outputAllocationRGB.copyTo(outBuffer)
-    //     return outBuffer
-    // }
-
-    // private fun getScriptLaunchOptions(width: Int, height: Int): Script.LaunchOptions {
-    //     /*
-    //      * These coordinates are the portion of the original image that we want to
-    //      * include.  Because we're rotating (in this case) x and y are reversed
-    //      * (but still offset from the actual center of each dimension)
-    //      */
-    //     return Script.LaunchOptions()
-    //         .setX(0, width)
-    //         .setY(0, height)
-    //     // return Script.LaunchOptions()
-    //     //     .setX(starty, endy)
-    //     //     .setY(startx, endx)
-    // }
 }
--- android/app/src/main/rs/crop.rs
+++ android/app/src/main/rs/crop.rs
@@ -2,29 +2,9 @@
 #pragma rs java_package_name(com.sicariusnoctis.collaborativeintelligence)
 #pragma rs_fp_relaxed
 
-rs_allocation output;
+rs_allocation input;
 uint32_t xStart, yStart;
 
-void RS_KERNEL crop(uchar4 in, uint32_t x, uint32_t y) {
-    rsSetElementAt_uchar4(output, in, x - xStart, y - yStart);
+uchar4 RS_KERNEL crop(uint32_t x, uint32_t y) {
+    return rsGetElementAt_uchar4(input, x + xStart, y + yStart);
 }
-
-/*
-rs_allocation inputFrame;
-rs_allocation outputFrame;
-
-uint32_t xStart, yStart;
-uint32_t outputWidth, outputHeight;
-
-uchar4 RS_KERNEL crop(uchar4 in, uint32_t x, uint32_t y) {
-    uchar Y = rsGetElementAtYuv_uchar_Y(inputFrame, x, y);
-    uchar U = rsGetElementAtYuv_uchar_U(inputFrame, x, y);
-    uchar V = rsGetElementAtYuv_uchar_V(inputFrame, x, y);
-
-    uchar4 rgba = rsYuvToRGBA_uchar4(Y, U, V);
-    rgba.a = 0xFF;
-
-    rsSetElementAt_uchar4(outputFrame, rgba, x - xStart, y - yStart);
-    return rgba;
-}
-*/
