a740c805e2616a4a1ecea589e0e48bacffec0e97
Mateen Ulhaq <mulhaq2005@gmail.com>
2019-09-06 06:22:12 -0700

    Forgot to include convert.rs and rotate.rs

---
+++ android/app/src/main/rs/convert.rs
@@ -0,0 +1,13 @@
+#pragma version(1)
+#pragma rs java_package_name(com.sicariusnoctis.collaborativeintelligence)
+#pragma rs_fp_relaxed
+
+rs_allocation output;
+uint32_t width;
+
+void RS_KERNEL rgba2rgb(uchar4 in, uint32_t x, uint32_t y) {
+    uint32_t pos = 3 * (x + width * y);
+    rsSetElementAt_uchar(output, in.r, pos);
+    rsSetElementAt_uchar(output, in.g, pos + 1);
+    rsSetElementAt_uchar(output, in.b, pos + 2);
+}
---
+++ android/app/src/main/rs/rotate.rs
@@ -0,0 +1,22 @@
+#pragma version(1)
+#pragma rs java_package_name(com.sicariusnoctis.collaborativeintelligence)
+#pragma rs_fp_relaxed
+
+rs_allocation input;
+uint32_t width;
+uint32_t height;
+
+uchar4 RS_KERNEL rotate90(uint32_t x, uint32_t y) {
+    const uchar4 *out = rsGetElementAt(input, y, width - 1 - x);
+    return *out;
+}
+
+uchar4 RS_KERNEL rotate270(uint32_t x, uint32_t y) {
+    const uchar4 *out = rsGetElementAt(input, height - 1 - y, x);
+    return *out;
+}
+
+uchar4 RS_KERNEL rotate180(uint32_t x, uint32_t y) {
+    const uchar4 *out = rsGetElementAt(input, width - x - 1, height - y - 1);
+    return *out;
+}
