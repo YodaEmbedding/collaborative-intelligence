0a14b1680bac9519a60babfa2a90e4c2c1caba71
Mateen Ulhaq <mulhaq2005@gmail.com>
2019-09-16 01:05:01 -0700

    Reuse socket address

--- server/main.py
+++ server/main.py
@@ -122,6 +122,7 @@ def main():
 
     if not DEBUG:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         sock.bind((IP, PORT))
         sock.listen(1)
     else:
