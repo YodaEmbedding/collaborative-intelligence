768b9803f067f193a9df97370c662c9a280d473f
Mateen Ulhaq <mulhaq2005@gmail.com>
2019-09-19 05:59:02 -0700

    Convert single quotes to double quotes

--- server/main.py
+++ server/main.py
@@ -17,23 +17,23 @@ import tensorflow as tf
 from tensorflow import keras
 
 spec = importlib.util.spec_from_file_location(
-    'model_def',
+    "model_def",
     os.path.expandvars(
-        '$HOME/code/experiments/py/tensorflow/resnet-keras-split/main.py'))
+        "$HOME/code/experiments/py/tensorflow/resnet-keras-split/main.py"))
 model_def = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(model_def)
 
-IP = '0.0.0.0'
+IP = "0.0.0.0"
 PORT = 5678
 
 def read_eol(conn):
-    return conn.recv(1, socket.MSG_WAITALL) == b'\x00'
+    return conn.recv(1, socket.MSG_WAITALL) == b"\x00"
 
 def read_fixed_message(conn) -> ByteString:
     msg_len_buf = conn.recv(4, socket.MSG_WAITALL)
     if len(msg_len_buf) != 4 or not read_eol(conn):
         return None
-    msg_len = int.from_bytes(msg_len_buf, byteorder='big')
+    msg_len = int.from_bytes(msg_len_buf, byteorder="big")
     buf = conn.recv(msg_len, socket.MSG_WAITALL)
     if len(buf) < msg_len or not read_eol(conn):
         return None
@@ -53,7 +53,7 @@ def str_preview(s, max_len=16):
 
     if len(s) < max_len:
         return s
-    return f'{unquoted(s[:max_len - 6])}...{unquoted(s[-3:])}'
+    return f"{unquoted(s[:max_len - 6])}...{unquoted(s[-3:])}"
 
 def decode_data(sess, model, data, dtype=tf.float32):
     input_shape = model.layers[1].input_shape[1:]
@@ -73,7 +73,7 @@ class SockMonkey:
         pass
 
     def accept(self):
-        return ConnMonkey(self._filename), ('Virtual Address', -1)
+        return ConnMonkey(self._filename), ("Virtual Address", -1)
 
 class ConnMonkey:
     """Simulate a connection using given file data cyclically."""
@@ -82,17 +82,17 @@ class ConnMonkey:
 
     def __init__(self, filename):
         self._pos = 0
-        with open(filename, 'rb') as f:
+        with open(filename, "rb") as f:
             self._data = f.read()
         self._data = (
-            len(self._data).to_bytes(4, byteorder='big') +
-            b'\x00' +
+            len(self._data).to_bytes(4, byteorder="big") +
+            b"\x00" +
             self._data +
-            b'\x00')
+            b"\x00")
 
     def recv(self, num_bytes, flags):
         if flags != socket.MSG_WAITALL:
-            raise ValueError('Unsupported flags')
+            raise ValueError("Unsupported flags")
         xs = []
         remaining = num_bytes
         while remaining > 0:
@@ -102,21 +102,21 @@ class ConnMonkey:
             self._pos %= len(self._data)
             remaining -= len(x)
             xs.append(x)
-        result = b''.join(xs)
+        result = b"".join(xs)
         return result
 
 class Main:
     def __init__(self, debug, dtype, model_name):
         self.dtype = dtype
 
-        opt_suffix = '-float' if dtype == tf.float32 else ''
-        model_filename = f'{model_name}-server{opt_suffix}.h5'
+        opt_suffix = "-float" if dtype == tf.float32 else ""
+        model_filename = f"{model_name}-server{opt_suffix}.h5"
 
-        print('Loading model...')
+        print("Loading model...")
         self.sess = tf.Session()
         self.model = keras.models.load_model(
             model_filename,
-            custom_objects={'DecoderLayer': model_def.DecoderLayer})
+            custom_objects={"DecoderLayer": model_def.DecoderLayer})
 
         if not debug:
             self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -124,28 +124,28 @@ class Main:
             self.sock.bind((IP, PORT))
             self.sock.listen(1)
         else:
-            self.sock = SockMonkey('resnet34-float-add_8-monitor.dat')
+            self.sock = SockMonkey("resnet34-float-add_8-monitor.dat")
 
     def start_connection(self):
-        print('Waiting for connection...')
+        print("Waiting for connection...")
         conn, addr = self.sock.accept()
-        print(f'Established connection on\n{conn}\n{addr}')
+        print(f"Established connection on\n{conn}\n{addr}")
 
         for i in itertools.count():
             fine, data, data_tensor = self._loop_once(i, conn)
             if not fine:
                 break
 
-        print('Closing connection...')
+        print("Closing connection...")
         conn.close()
 
         if data is None or data_tensor is None:
             return
 
         with suppress(NameError):
-            with open('last_run_final_frame.dat', 'wb') as f:
+            with open("last_run_final_frame.dat", "wb") as f:
                 f.write(data)
-            np.save('last_run_final_frame.npy', data_tensor)
+            np.save("last_run_final_frame.npy", data_tensor)
 
     def _loop_once(self, i, conn):
         t0 = time.time()
@@ -165,30 +165,30 @@ class Main:
 
         t3 = time.time()
         decoded_pred = imagenet_utils.decode_predictions(predictions)[0]
-        decoded_pred_str = '\n'.join(
-            f'{name:12} {desc:24} {score:0.3f}'
+        decoded_pred_str = "\n".join(
+            f"{name:12} {desc:24} {score:0.3f}"
             for name, desc, score in decoded_pred)
-        conn.send(f'{i} {decoded_pred}\n'.encode('utf8'))
+        conn.send(f"{i} {decoded_pred}\n".encode("utf8"))
 
         t4 = time.time()
 
-        print(now.isoformat(sep=' ', timespec='milliseconds'))
+        print(now.isoformat(sep=" ", timespec="milliseconds"))
         print(i, len(data), str_preview(data))
         print(decoded_pred_str)
-        print(f'Read:       {1000 * (t1 - t0):4.0f} ms')
-        print(f'Feed input: {1000 * (t2 - t1):4.0f} ms')
-        print(f'Inference:  {1000 * (t3 - t2):4.0f} ms')
-        print(f'Send:       {1000 * (t4 - t3):4.0f} ms')
-        print(f'Total:      {1000 * (t4 - t0):4.0f} ms')
-        print('')
+        print(f"Read:       {1000 * (t1 - t0):4.0f} ms")
+        print(f"Feed input: {1000 * (t2 - t1):4.0f} ms")
+        print(f"Inference:  {1000 * (t3 - t2):4.0f} ms")
+        print(f"Send:       {1000 * (t4 - t3):4.0f} ms")
+        print(f"Total:      {1000 * (t4 - t0):4.0f} ms")
+        print("")
 
         return True, data, data_tensor
 
 def main():
     DEBUG = False
     DTYPE = tf.uint8
-    MODEL_NAME = 'resnet34'
-    # MODEL_NAME = 'mobilenet_v1_1.0_224'
+    MODEL_NAME = "resnet34"
+    # MODEL_NAME = "mobilenet_v1_1.0_224"
 
     main = Main(DEBUG, DTYPE, MODEL_NAME)
 
@@ -201,7 +201,7 @@ def main():
             except BrokenPipeError as e:
                 print(e)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
 
 # TODO
