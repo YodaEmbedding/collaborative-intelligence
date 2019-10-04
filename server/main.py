cee6799a86a99299482033f65409d8dd00772e43
Mateen Ulhaq <mulhaq2005@gmail.com>
2019-10-03 23:46:36 -0700

    ModelConfig hashable by asdict upon JSON representation

--- server/main.py
+++ server/main.py
@@ -9,7 +9,7 @@ import os
 import time
 import traceback
 from asyncio import StreamReader, StreamWriter
-from dataclasses import dataclass
+from dataclasses import asdict, dataclass
 from datetime import datetime
 from typing import (
     Any,
@@ -91,20 +91,22 @@ class ModelConfig:
     layer: str
     encoder: str
     decoder: str
-    encoder_args: str
-    decoder_args: str
+    encoder_args: Dict[str, Any]
+    decoder_args: Dict[str, Any]
 
     def to_path(self) -> str:
         enc = ModelConfig._encdec_str(self.encoder, self.encoder_args)
         dec = ModelConfig._encdec_str(self.decoder, self.decoder_args)
         return f"../tools/{self.model}/{self.model}-{self.layer}-{enc}-{dec}"
 
+    def __hash__(self) -> int:
+        return hash(json.dumps(asdict(self)))
+
     @staticmethod
     def _encdec_str(name, args):
         if len(args) == 0:
             return name
-        # return f"{name}({dict_to_str(args)})"
-        return f"{name}({args})"
+        return f"{name}({dict_to_str(args)})"
 
 
 @dataclass
@@ -278,15 +280,14 @@ async def produce(reader: StreamReader, putter):
     model_config: ModelConfig = None
 
     # TODO DEBUG
+    # TODO how to format floats/tuples?
     model_config = ModelConfig(
         "resnet34",
         "add_8",
         "UniformQuantizationU8Encoder",
         "UniformQuantizationU8Decoder",
-        dict_to_str(
-            {"clip_range": (-1.0, 1.0)}
-        ),  # TODO how to format floats/tuples?
-        dict_to_str({"clip_range": (-1.0, 1.0)}),
+        {"clip_range": (-1.0, 1.0)},
+        {"clip_range": (-1.0, 1.0)},
     )
     await putter((model_config, "acquire", None))
 
